e65a44e840b72f9e4ed2a7199686af12ea0554a3
elaindias16 <elainediaspires16@gmail.com>
2023-03-14 17:19:51 -0300

    aula de event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -11,6 +11,9 @@ export class DataBindingComponent {
   url: string = 'http://loiane.com';
   cursoAngular : boolean = true;
   urlImage: string = 'https://picsum.photos/200'
+  valorAtual: string  = ''
+  valorSalvo : string = ''
+  isMouseOver : boolean = false;
 
 
   getValor(){
@@ -20,6 +23,25 @@ export class DataBindingComponent {
   getCurtirCurso(){
     return true;
   }
+
+  botaoClicado(){
+    alert('botao clicado!');
+  }
+
+  onKeyUp(evento : KeyboardEvent){
+    console.log(evento)
+    console.log( (<HTMLInputElement>evento.target).value );
+    this.valorAtual = (<HTMLInputElement>evento.target).value
+  }
+
+  salvarValor(valor : string){
+    this.valorSalvo = valor
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
 
   constructor(){
 
