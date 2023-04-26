4895915e1a3e4f1c73606e27f3a6d32bd156c09e
elaindias16 <elainediaspires16@gmail.com>
2023-04-26 18:45:05 -0300

    routes

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { routing } from './app.routing';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos/cursos.service';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 
 
 @NgModule({
@@ -15,7 +18,9 @@ import { routing } from './app.routing';
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +28,7 @@ import { routing } from './app.routing';
     routing
 
   ],
-  providers: [],
+  providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- rotas/src/app/app.routing.ts
+++ rotas/src/app/app.routing.ts
@@ -4,11 +4,17 @@ import { Route, RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 import { CursosComponent } from "./cursos/cursos.component";
+import { CursoDetalheComponent } from "./curso-detalhe/curso-detalhe.component";
+import { CursoNaoEncontradoComponent } from "./curso-nao-encontrado/curso-nao-encontrado.component";
 
 const APP_ROUTES : Routes = [
     {path : 'login', component : LoginComponent },
     {path : '', component : HomeComponent },
     {path : 'cursos', component : CursosComponent },
+    {path : 'curso/:id', component : CursoDetalheComponent },
+    {path : 'naoEncontrado', component : CursoNaoEncontradoComponent },
+
+
 
 ];
 
